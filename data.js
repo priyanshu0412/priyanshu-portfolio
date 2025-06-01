ac3ba63c20f3cee57d656e5dc8a7251f5ec5642b
priyanshu0412 <pdevops04@gmail.com>
2025-06-01 21:55:56 +0530

    first commit implementation

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ import { aboutMeImg, img, ProjectImgCover } from "@/assets"
 
 export const Navbar_data = [
     {
-        name: "Home 1 "
+        name: "Home"
     },
     {
         name: "About"
